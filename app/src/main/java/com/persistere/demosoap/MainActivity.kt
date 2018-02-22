6ebcb6501836545847bb3dc7c1a5b582a1b0c2bc
logonrm <logonrm@fiap.com.br>
2018-02-21 21:02:13 -0300

    Criando servico SOAP

--- app/src/main/java/com/persistere/demosoap/MainActivity.kt
+++ app/src/main/java/com/persistere/demosoap/MainActivity.kt
@@ -3,6 +3,11 @@ package com.persistere.demosoap
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.ksoap2.SoapEnvelope
+import org.ksoap2.serialization.PropertyInfo
+import org.ksoap2.serialization.SoapObject
+import org.ksoap2.serialization.SoapSerializationEnvelope
+import org.ksoap2.transport.HttpTransportSE
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     private val soapAction = nameSpace + methodName
     private val paramentro1 = "num1"
     private val paramentro2 = "num2"
-    private val parametro3 = "op"
+    private val paramentro3 = "op"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +26,46 @@ class MainActivity : AppCompatActivity() {
 
     inner class CallWebService : AsyncTask<String, Void, String> () {
         override fun doInBackground(vararg params: String?): String {
+            var result = ""
+            val soapObject = SoapObject(nameSpace, methodName)
+            val numberInfo = PropertyInfo()
 
+            numberInfo.name = paramentro1
+            numberInfo.value = params[0]
+            numberInfo.type = Integer::class.java
+
+            soapObject.addProperty(numberInfo)
+
+
+            val number2Info = PropertyInfo()
+            number2Info.name = paramentro2
+            number2Info.value = params[1]
+            number2Info.type = Integer::class.java
+
+            soapObject.addProperty(number2Info)
+
+            val opInfo = PropertyInfo()
+            opInfo.name = paramentro3
+            opInfo.value = params[2]
+            opInfo.type = String::class.java
+
+            soapObject.addProperty(opInfo)
+
+            val envelope = SoapSerializationEnvelope(SoapEnvelope.VER11)
+            envelope.setOutputSoapObject(soapObject)
+
+            val httpTransportSE = HttpTransportSE(url)
+
+            try {
+                httpTransportSE.call(soapAction, envelope)
+                val soapPrimitive = envelope.response
+                result = soapPrimitive.toString()
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return result
         }
 
     }
